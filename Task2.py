591132c0a12757c3c1af611bc5f9f2a52a405a68
Tark <113023722+TarkP@users.noreply.github.com>
2022-09-07 16:37:15 +0200

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,18 @@
+Mat=["sås","äppelsås","sopa"]
+# really good sauce, applesauce and soup. Mat means food
+print(*Mat, sep="\n")
+
+Cleaningwipes="babywipes bluewipes greenwipes redwipes"
+underwear="Boxers and Panties"
+# using to clean your underwear and of course underwear is underwear
+print("\nI love using",Cleaningwipes,"to clean my", underwear, sep=" ")
+
+# Means Hello
+print ("hello world")
+# just printing something
+print ("printing...")
+
+# Cleaning the table glass is important
+print ("\nCleaning the table", end=" Glass")
+#cleaning the window door is hard 
+print(" and the window", end=" Door")
