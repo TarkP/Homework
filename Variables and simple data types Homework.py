29ac9823c7aaaae95d12a280ffd76fe89a6e0c55
Tark <113023722+TarkP@users.noreply.github.com>
2022-09-10 00:30:59 +0200

    Add files via upload

---
+++ Variables and simple data types Homework.py
@@ -0,0 +1,21 @@
+#Task 1
+name = "Tark"
+day = "9 September"
+print ("good day " + name + "!"  ,  day + " is a good day for learning Python. " ) 
+
+#Task 2
+First_name = "Tark"
+Last_name = "Karlsson"
+print ("greetings " + First_name, Last_name)
+
+#Task 3
+A = 1
+B = 2
+
+print (A + B)
+print (B - A)
+print (B / A)
+print (B * A)
+print (B ** A)
+print (A % B)
+print (B // A)
