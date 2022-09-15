49a419d26c71f54fa653146decaf4add4d3d444f
Tark <113023722+TarkP@users.noreply.github.com>
2022-09-15 19:30:10 +0200

    Add files via upload

---
+++ Homework 3.py
@@ -0,0 +1,25 @@
+name = "Dev Code"
+
+first_name = name[:2]
+last_name = name[-2:]
+print(first_name + last_name)
+#task 2
+phone_number = input ("please enter your phone number")
+if len (phone_number) ==10:
+    print("Phone number accepted, thank you!")
+elif len (phone_number) !=10:
+    print ("invalid phone number")
+#task 3
+name = "tark"
+my_name = input ("Enter your name")
+
+if my_name.lower() == name:
+    print ("name is correct")
+else:
+    print ("name is invalid")
+
+
+
+
+
+
